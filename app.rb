042fd386e06c3e59eaff8ac3753ca84dc8b81c84
mallorymaher <mallory.maher@comcast.net>
2018-08-14 19:13:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+ get '/' do
+   erb :index
+ end
+ 
+ get '/elana' do
+   erb :elana
+ end
+ 
+ get '/mallory' do
+   erb :mallory
+ end
 
 end
